32da0c37eca6212255d6bd3ec26647d948f80722
VinayKumar <vinaydarla832@gmail.com>
2026-05-09 18:05:25 +0530

    added Roboshop project configuration

--- roboshop-dev-infra/40-databases/bootstrap.sh
+++ roboshop-dev-infra/40-databases/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+component=$1
 dnf install ansible -y
-ansible-pull -U https://github.com/Darla-Vinay-Kumar/Ansible_Roboshop_Roles-tf.git -e component=mongodb main.yml -i inventory.ini
+ansible-pull -U https://github.com/Darla-Vinay-Kumar/Ansible_Roboshop_Roles-tf.git -e component=$component main.yml -i inventory.ini
