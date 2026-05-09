7b5eaac03af2009f00f58522e9a0e9268f3ffce8
VinayKumar <vinaydarla832@gmail.com>
2026-05-09 18:29:07 +0530

    added Roboshop project configuration

--- roboshop-dev-infra/40-databases/locals.tf
+++ roboshop-dev-infra/40-databases/locals.tf
@@ -8,6 +8,8 @@ locals {
 
     redis_sg_ids = data.aws_ssm_parameter.redis_sg_id.value
 
+    rabbitmq_sg_ids = data.aws_ssm_parameter.rabbitmq_sg_id.value
+
     common_tags = {
         project = var.project_name
         Environment = var.environment
