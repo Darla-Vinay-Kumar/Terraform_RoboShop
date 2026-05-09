e4920b3c81492f46ee29464def2f467b34982718
VinayKumar <vinaydarla832@gmail.com>
2026-05-09 18:29:17 +0530

    added Roboshop project configuration

--- roboshop-dev-infra/30-sg-rules/main.tf
+++ roboshop-dev-infra/30-sg-rules/main.tf
@@ -25,3 +25,12 @@ resource "aws_security_group_rule" "mongodb_from_bastion" {
     protocol = "tcp"
     to_port = 22
 }
+# redis accepting traffic from bastion
+resource "aws_security_group_rule" "redis_from_bastion" {
+    type = "ingress"
+    security_group_id = local.redis_sg_id
+    source_security_group_id = local.bastion_sg_id
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+}
