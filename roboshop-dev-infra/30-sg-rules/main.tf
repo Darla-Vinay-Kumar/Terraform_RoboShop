4645172e0addd4b81a2ac997ced05a6d7e4e7936
VinayKumar <vinaydarla832@gmail.com>
2026-05-10 16:45:51 +0530

    terraform

--- roboshop-dev-infra/30-sg-rules/main.tf
+++ roboshop-dev-infra/30-sg-rules/main.tf
@@ -43,5 +43,13 @@ resource "aws_security_group_rule" "rabbitmq_from_bastion" {
     protocol = "tcp"
     to_port = 22
 }
-
+# mysql accepting traffic from bastion
+resource "aws_security_group_rule" "mysql_from_bastion" {
+    type = "ingress"
+    security_group_id = local.mysql_sg_id
+    source_security_group_id = local.bastion_sg_id
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+}
 
